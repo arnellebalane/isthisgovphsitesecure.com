3aa47c4b2deb11c4a094ea834be693b167e56873
Arnelle Balane <arnellebalane@gmail.com>
2021-06-15 02:38:44 +0800

    Add last update date in Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,26 @@
 <template>
   <header class="Header">
     <h1>Is this gov.ph site <span>secure?</span></h1>
+
+    <p>
+      Last update: <time datetime="meta.lastUpdate">{{ lastUpdate }}</time>
+    </p>
   </header>
 </template>
 
+<script setup>
+import dayjs from 'dayjs';
+import meta from '@data/meta.json';
+
+const lastUpdate = dayjs(meta.lastUpdate).format('MMMM D, YYYY hh:mm:ss A');
+</script>
+
 <style scoped>
 header {
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  flex-wrap: wrap;
   padding: 2.4rem 0;
 }
 
@@ -20,12 +35,17 @@ span {
 
 @media (max-width: 560px) {
   header {
+    justify-content: center;
     padding: 2.4rem 1.2rem;
   }
 
   h1 {
     font-size: 2.4rem;
     text-align: center;
+  }
+
+  p {
+    font-size: 1.4rem;
   }
 }
 </style>
